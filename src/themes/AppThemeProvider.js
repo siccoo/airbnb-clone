fcf412c1922b0929da3784b3ebd2580352a54059
Michael Chilaka <obichilaka@gmail.com>
2022-10-17 22:26:47 +0100

    setup of airbnb react project, installing our npm packages, updates on readme.md & AppThemeProvider.js

---
+++ src/themes/AppThemeProvider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const AppThemeProvider = () => {
+  return (
+    <div>AppThemeProvider</div>
+  )
+}
+
+export default AppThemeProvider
